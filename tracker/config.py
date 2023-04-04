d5d3a241d9471aec85117ce51d8806b1296a1fc3
alizademhdi <alizademhdi@gmail.com>
2023-04-04 16:30:43 +0330

    Fix args in tracker

--- tracker/config.py
+++ tracker/config.py
@@ -1,5 +1,5 @@
 from decouple import config
 
 LOG_PATH = config("LOG_PATH", "./log/tracker.log")
-SERVER_ADDRESS = config("ADDRESS", "")
+SERVER_ADDRESS = config("ADDRESS", "127.0.0.1")
 SERVER_PORT = config("PORT", 6771)
--- tracker/main.py
+++ tracker/main.py
@@ -1,7 +1,12 @@
 from server import run_server
 import asyncio
 from config import *
+import sys
 
 if __name__ == '__main__':
+    args = sys.argv[1:]
+    SERVER_ADDRESS = args[0].split(':')[0]
+    SERVER_PORT = args[0].split(':')[1]
+
     asyncio.run(run_server(host=SERVER_ADDRESS,
                            port=SERVER_PORT))
